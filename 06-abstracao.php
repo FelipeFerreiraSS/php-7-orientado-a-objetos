07d9eaac274af8681a052e930bb12ade661c7f3c
Felipe Ferreira <filipiferreirasilva789@gmail.com>
2020-12-21 14:34:14 -0300

    Update 06-abstracao.php

--- 06-abstracao.php
+++ 06-abstracao.php
@@ -7,29 +7,35 @@ abstract class Banco {
 	protected $limiteSaque;
 	protected $juros;
 
-	abstract protected function Sacar();
+	public function setSaldo($s){
+		$this->saldo = $s;
+	}
 
-	abstract protected function Depositar();
+	public function getSaldo(){
+		return $this->saldo;
+	}
+
+	abstract protected function Sacar($s);
+
+	abstract protected function Depositar($d);
 }
 
 class Itau extends Banco {
-	public function Sacar() {
-		echo "Sacou";
+	public function Sacar($s) {
+		$this->saldo -= $s;
+		echo "<hr> Sacou: ".$s;
 	}
 
-	public function Depositar() {
-		echo "Depositou";
-	}
-}
-
-class Bradesco extends Banco {
-	public function Sacar() {
-		echo "Sacou";
-	}
-
-	public function Depositar() {
-		echo "Depositou";
+	public function Depositar($d) {
+		$this->saldo += $d;
+		echo "<hr> Depositou: ".$d;
 	}
 }
 
 $itau = new Itau();
+$itau->setSaldo(1000);
+echo "<hr> Saldo: ".$itau->getSaldo();
+$itau->Sacar(250);
+echo "<hr> Saldo: ".$itau->getSaldo();
+$itau->Depositar(900);
+echo "<hr> Saldo: ".$itau->getSaldo();
